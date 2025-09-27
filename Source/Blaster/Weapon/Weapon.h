c63629da6c165ca48b1a4e869a17b90eeba6433a
zitian <zitianchen01@gmail.com>
2025-09-28 05:57:43 +0800

    Add some stuff

---
+++ Source/Blaster/Weapon/Weapon.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Weapon.generated.h"
+
+UENUM(BlueprintType)
+enum class EWeaponState : uint8
+{
+	EWS_Initial UMETA(DisplayName = "Initial State"),
+	EWS_Equipped UMETA(DisplayName = "Equipped"),
+	EWS_Dropped UMETA(DisplayName = "Dropped"),
+
+	EWS_MAX UMETA(DisplayName = "DefaultMAX")
+};
+
+UCLASS()
+class BLASTER_API AWeapon : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AWeapon();
+	virtual void Tick(float DeltaTime) override;
+
+protected: 
+	virtual void BeginPlay() override;
+
+	virtual void OnsphereOverlap(
+		UPrimitiveComponent* OverlappedComponent, 
+		AActor* OtherActor, 
+		UPrimitiveComponent* OtherComp, 
+		int32 OtherBodyIndex, 
+		bool bFromSweep, const 
+		FHitResult& SweepResult);
+
+private: 
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+	USkeletalMeshComponent* WeaponMesh;
+
+	UPROPERTY(VisibleAnyWhere, Category = "Weapon Properties")
+	class USphereComponent* AreaSphere;
+
+	UPROPERTY(VisibleAnyWhere)
+	EWeaponState WeaponState;
+
+	UPROPERTY(VisibleAnyWhere, Category = "Weapon Properties")
+	class UWidgetComponent* PickupWidget;
+};
